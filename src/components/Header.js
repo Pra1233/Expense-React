a74d0ddce97c755825d4212c52ebb7cbbbff7534
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-12-02 20:19:22 +0530

    Verify User Email
    Task5

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,36 @@ import classes from "./Expense.module.css";
 
 const Header = () => {
   const authCtx = useContext(AuthContext);
+  const key = "AIzaSyBsDLPBGuT6kOlPdPc5f-SeSOz-Xdjyj0s";
   const onLogout = () => {
     authCtx.logout();
     localStorage.removeItem("token");
+  };
+
+  const verifyEmail = async () => {
+    try {
+      const res = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${key}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: authCtx.token,
+          }),
+        }
+      );
+
+      if (res.ok) {
+        const data = await res.json();
+        console.log("Verification email sent successfully to:", data.email);
+        console.log(data);
+      } else {
+        const data = await res.json();
+        console.error("Error Sending Verification Email", data.error.message);
+      }
+    } catch (e) {
+      console.error("Error", e.message);
+    }
   };
 
   return (
@@ -21,6 +48,11 @@ const Header = () => {
       {authCtx.isloggedin && (
         <button onClick={onLogout} className={classes.logout}>
           Logout
+        </button>
+      )}
+      {authCtx.isloggedin && (
+        <button className={classes.logout} onClick={verifyEmail}>
+          Verify Email
         </button>
       )}
     </div>
